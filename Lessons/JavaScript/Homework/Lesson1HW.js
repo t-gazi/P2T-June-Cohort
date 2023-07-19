cd9bb783a460cb8f9f4e40fb8b2a9da2e11905e1
t-gazi <t.gazi786@gmail.com>
2023-07-19 21:51:32 +0100

    Starting JavaScript

---
+++ Lessons/JavaScript/Homework/Lesson1HW.js
@@ -0,0 +1,21 @@
+//q.1 Using string concatenation add two different strings together and print this off.
+
+console.log ("Hello" + " " + "World!")
+
+//.q2 Using variables create a variable for a number, a string. Then print each of these variables.
+
+var firstword = "Hello"
+var secondword = 2
+
+console.log(firstword)
+console.log(secondword)
+
+/*q.3 Using string*/
+
+var name = "tasnia"
+var age = 27
+var dob = 95
+
+console.log("my name is" , name, "I am years", age, "old and I was born in" , dob)
+
+
---
+++ Lessons/JavaScript/Lessons/Lesson1.js
@@ -0,0 +1,18 @@
+//this is a single line comment
+
+/*This
+will be a
+multi-line
+comment*/
+
+console.log("Hello you have executed the lesson 1 file");
+
+console.log("hello world");
+
+var greeting = "hello";
+var myName = "tasnia";
+
+
+console.log(greeting+" "+myName);
+console.log(1234+5678)
+
